d7c69aeb38dff31466ecfffbd70a1ec611d1010a
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-04 23:54:21 +0700

    refactor: table

--- database/migrations/2025_04_04_164755_blog.php
+++ database/migrations/2025_04_04_164755_blog.php
@@ -12,6 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('images');
+            $table->string('title');
+            $table->string('body');
+            $table->timestamp('created_at');
+        });
     }
 
     /**
@@ -20,5 +28,6 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::dropIfExists('blog');
     }
 };
